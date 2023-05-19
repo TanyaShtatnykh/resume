be6b4dbc38ff36910d6725b107be45e76384efc0
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2023-05-19 22:25:50 +0700

    refactoring

--- src/blocks/info/info.js
+++ src/blocks/info/info.js
@@ -1,20 +1,10 @@
 $(function () {
   $('.info__title').on('click', function () {
-    if ($(this).hasClass('info__title--open')) {
-      $(this).removeClass('info__title--open');
-      $(this).next('.info__list').removeClass('info__list--open');
-    } else {
+    if (!$(this).hasClass('info__title--open')) {
+      $('.info__wrapper').removeClass('info__wrapper--open');
       $('.info__title').removeClass('info__title--open');
-      $('.info__list').removeClass('info__list--open');
-      $(this).addClass('info__title--open');
-      $(this).next('.info__list').addClass('info__list--open');
     }
+    $(this).toggleClass('info__title--open');
+    $(this).next('.info__wrapper').toggleClass('info__wrapper--open');
   });
-
-  // $(document).on('click', function (e) {
-  //   if ( ! ('.block').is(e.target) && listItem.has(e.target).length === 0) {
-  //     $('.production__stage-text').removeClass('production__stage-text--open');
-  //     $('.production__stage-title').removeClass('production__stage-title--open');
-  //   }
-  // })
 });
